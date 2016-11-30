f1212e884801205b4d6b66631b0937c57700a9dc
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-30 17:37:38 +0000

    heroku pt.7

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link
   property :url, String
 end
 
-  DataMapper.setup(:default, ENV["https://bookmark-manager-sultan.herokuapp.com/"] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, ENV["postgres://actqppofxlpjjb:jbKNh2ji5nyyWkUgU2Hez6XHld@ec2-54-163-226-30.compute-1.amazonaws.com:5432/dbcqnb3f4lhf7l"] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
 DataMapper::Logger.new($stdout, :debug)
 
