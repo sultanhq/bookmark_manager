13e2fb0f2c6a55683c048691cd124dabaf810b82
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-30 15:58:52 +0000

    exercise 11

--- app/app.rb
+++ app/app.rb
@@ -11,7 +11,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    # Link.create(title: params[:title], url: params[:url])
+    Link.create(title: params[:title], url: params[:url])
     redirect to('/links')
   end
 
