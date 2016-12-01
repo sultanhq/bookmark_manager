75399fdb45d6b638c10447c95464b0412d4c35b0
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-01 15:49:42 +0000

    WORKING

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,4 +6,4 @@ require_relative 'models/tag'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
